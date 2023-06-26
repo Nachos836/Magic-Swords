3820dba5633fb1a2c06f8bbb96e888c1f2fb9f67
Cyka <nachos636@gmail.com>
2023-06-26 17:59:04 +0300

    [HOTFIX] SceneSwitch -> SceneLoader

---
+++ Assets/Features/Miscellaneous/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace MagicSwords.Features.Miscellaneous
+{
+    internal interface ISceneLoader
+    {
+        UniTask LoadAlongsideAsync(int buildIndex, CancellationToken cancellation = default);
+        UniTask TransferToAsync(int buildIndex, CancellationToken cancellation = default);
+    }
+
+    internal sealed class SceneLoader : ISceneLoader
+    {
+        async UniTask ISceneLoader.LoadAlongsideAsync(int buildIndex, CancellationToken cancellation)
+        {
+            await SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Additive)
+                .WithCancellation(cancellation)
+                .SuppressCancellationThrow();
+        }
+
+        async UniTask ISceneLoader.TransferToAsync(int buildIndex, CancellationToken cancellation)
+        {
+            await SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Single)
+                .WithCancellation(cancellation)
+                .SuppressCancellationThrow();
+        }
+    }
+}
