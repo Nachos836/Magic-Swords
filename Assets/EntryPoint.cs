a26d42832dcf89ab5e7b5381086c2518f6eb66e0
Cyka <nachos636@gmail.com>
2023-06-26 18:00:38 +0300

    [CLEANUP] Move EntryPoint to Project Origin

---
+++ Assets/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+
+namespace MagicSwords
+{
+    using Features.Miscellaneous;
+
+    internal sealed class EntryPoint : IAsyncStartable, IDisposable
+    {
+        private readonly ISceneLoader _sceneLoader;
+        private readonly int _mainMenu;
+
+        public EntryPoint(ISceneLoader sceneLoader, int mainMenu)
+        {
+            _sceneLoader = sceneLoader;
+            _mainMenu = mainMenu;
+        }
+
+        async UniTask IAsyncStartable.StartAsync(CancellationToken cancellation)
+        {
+            UnityEngine.Debug.Log("Ура, мы начали проект!!!");
+            
+            await _sceneLoader.LoadAlongsideAsync(_mainMenu, cancellation);
+        }
+
+        void IDisposable.Dispose()
+        {
+            UnityEngine.Debug.Log("Пока!!");
+        }
+    }
+}
