6739e6b9c23758daa319116869fe4ced0309a8f5
Cyka <nachos636@gmail.com>
2023-06-26 22:21:10 +0300

    [HOTFIX] Adjust Application Entry Flow

---
+++ Assets/DI/ApplicationEntry/Dependencies/MessagePipeDependencies.cs
@@ -0,0 +1,24 @@
+﻿using MessagePipe;
+using VContainer;
+
+namespace MagicSwords.DI.ApplicationEntry.Dependencies
+{
+    public static class MessagePipeDependencies
+    {
+        public static IContainerBuilder AddMessagePipeFeature(this IContainerBuilder builder, out MessagePipeOptions options)
+        {
+            options = builder.RegisterMessagePipe(configure: pipeOptions =>
+            {
+#               if DEBUG
+                // EnableCaptureStackTrace slows performance
+                // Recommended to use only in DEBUG and in profiling, disable it.
+                pipeOptions.EnableCaptureStackTrace = true;
+#               endif
+            });
+
+            builder.RegisterBuildCallback(resolver => GlobalMessagePipe.SetProvider(resolver.AsServiceProvider()));
+
+            return builder;
+        }
+    }
+}
---
+++ Assets/DI/ApplicationEntry/Dependencies/SceneLoaderDependencies.cs
@@ -0,0 +1,16 @@
+﻿using VContainer;
+
+namespace MagicSwords.DI.ApplicationEntry.Dependencies
+{
+    using Features.Miscellaneous;
+
+    internal static class SceneLoaderDependencies
+    {
+        public static IContainerBuilder AddSceneLoaderFeature(this IContainerBuilder builder)
+        {
+            builder.Register<ISceneLoader, SceneLoader>(Lifetime.Scoped);
+
+            return builder;
+        }
+    }
+}
