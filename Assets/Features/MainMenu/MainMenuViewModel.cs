ea1f8a8d098d3997b5e970b6edee988752b08b76
Anastasia <timmikvo@gmail.com>
2023-06-27 12:44:15 +0300

    [FEATURE] Add behaviour to main menu

---
+++ Assets/Features/MainMenu/MainMenuViewModel.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using ZBase.Foundation.Mvvm.ComponentModel;
+using ZBase.Foundation.Mvvm.Input;
+
+namespace MagicSwords.Features.MainMenu
+{
+    internal partial class MainMenuViewModel : MonoBehaviour, IObservableObject
+    {
+        [ObservableProperty] private bool _playing;
+
+        [RelayCommand]
+        private void OnSetPlayState()
+        {
+            Playing = !Playing;
+        }
+        
+        [RelayCommand]
+        private void OnSetExit()
+        {
+            Application.Quit();
+        }
+    }
+}
