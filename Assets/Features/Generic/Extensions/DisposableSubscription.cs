3ea5363105478f2743ad66088126cb91883c1391
Cyka <nachos636@gmail.com>
2023-07-04 18:06:28 +0300

    [INITIAL] Magic Swords

---
+++ Assets/Features/Generic/Extensions/DisposableSubscription.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MessagePipe;
+using Unity.Burst;
+
+namespace MagicSwords.Features.Generic.Extensions
+{
+    internal static class DisposableSubscription
+    {
+        public static DisposableBagBuilder Append(this DisposableBagBuilder builder, IDisposable income)
+        {
+            builder.Add(income);
+
+            return builder;
+        }
+
+        [BurstCompile]
+        public readonly struct SubscriptionHandler : IDisposable
+        {
+            private readonly Action _unsubscribe;
+
+            public SubscriptionHandler(Action unsubscribe) => _unsubscribe = unsubscribe;
+
+            void IDisposable.Dispose() => _unsubscribe.Invoke();
+        }
+    }
+}
