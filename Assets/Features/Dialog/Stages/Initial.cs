73a2339f6c7bd6d4ab1aaa7a4505e7229f6ec570
Cyka <nachos636@gmail.com>
2023-07-13 00:31:37 +0300

    [REFACTOR] Dialog: Setup -> Initial Stage

---
+++ Assets/Features/Dialog/Stages/Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace MagicSwords.Features.Dialog.Stages
+{
+    using Generic.Sequencer;
+    using Payload;
+
+    using Option = Generic.Functional.OneOf
+    <
+        Generic.Sequencer.IStage,
+        Generic.Sequencer.Stage.Canceled,
+        Generic.Sequencer.Stage.Errored
+    >;
+
+    public sealed class Initial : IStage, IStage.IProcess
+    {
+        private readonly Func<Message, Print> _autoPrintResolver;
+        private readonly string[] _monologue;
+
+        public Initial(Func<Message, Print> autoPrintResolver, string[] monologue)
+        {
+            _autoPrintResolver = autoPrintResolver;
+            _monologue = monologue;
+        }
+
+        UniTask<Option> IStage.IProcess.ProcessAsync(CancellationToken cancellation)
+        {
+            if (cancellation.IsCancellationRequested) return new UniTask<Option>(Stage.Cancel);
+
+            return new UniTask<Option>(Option.From(_autoPrintResolver.Invoke(new Message(_monologue))));
+        }
+    }
+}
