45da1373b17efc0a7acb97a8a03cd6c4042c4e2f
Cyka <nachos636@gmail.com>
2023-07-12 13:35:14 +0300

    [FEATURE] Add Animator-based State Machine

---
+++ Assets/Features/Generic/AnimatorStateMachine/State.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace MagicSwords.Features.Generic.AnimatorStateMachine
+{
+    public abstract class State : StateMachineBehaviour
+    {
+        protected StateMachine.IController _controller;
+        private bool _isInitialized;
+		private bool _isFirstEnter = true;
+
+        protected event Action FirstTimeEntered;
+        protected event Action Entered;
+        protected event Action Updated;
+        protected event Action Exited;
+
+        public void Construct(StateMachine.IController controller)
+        {
+            if (AssertIsNotInitialized() is false) return;
+
+            _controller = controller;
+
+            _isInitialized = true;
+        }
+
+        public sealed override void OnStateEnter
+        (
+            Animator animator,
+            AnimatorStateInfo stateInfo,
+            int layerIndex,
+            AnimatorControllerPlayable controller
+        ) {
+            if (AssertIsInitialized() is false) return;
+
+            base.OnStateEnter(animator, stateInfo, layerIndex, controller);
+
+            if (_isFirstEnter)
+            {
+                _isFirstEnter = false;
+
+                FirstTimeEntered?.Invoke();
+            }
+            Entered?.Invoke();
+        }
+
+        public sealed override void OnStateUpdate
+        (
+            Animator animator,
+            AnimatorStateInfo stateInfo,
+            int layerIndex,
+            AnimatorControllerPlayable controller
+        ) {
+            if (AssertIsInitialized() is false) return;
+
+            base.OnStateUpdate(animator, stateInfo, layerIndex, controller);
+
+            Updated?.Invoke();
+        }
+
+        public sealed override void OnStateExit
+        (
+            Animator animator,
+            AnimatorStateInfo stateInfo,
+            int layerIndex,
+            AnimatorControllerPlayable controller
+        ) {
+            if (AssertIsInitialized() is false) return;
+
+            base.OnStateExit(animator, stateInfo, layerIndex, controller);
+
+            Exited?.Invoke();
+        }
+
+        public sealed override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+        }
+
+        public sealed override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateUpdate(animator, stateInfo, layerIndex);
+        }
+
+        public sealed override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateExit(animator, stateInfo, layerIndex);
+        }
+
+        private bool AssertIsInitialized()
+        {
+            var claim = $"{GetType().Name} is not initialized";
+            return _isInitialized ? true : throw new InvalidOperationException(claim);
+        }
+
+        private bool AssertIsNotInitialized()
+        {
+            var claim = $"{GetType().Name} is already initialized";
+            return !_isInitialized ? true : throw new InvalidOperationException(claim);
+        }
+    }
+}
---
+++ Assets/Features/Generic/AnimatorStateMachine/StateMachine.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace MagicSwords.Features.Generic.AnimatorStateMachine
+{
+    [RequireComponent(typeof(Animator))]
+    public sealed class StateMachine : MonoBehaviour, StateMachine.IController
+    {
+        [SerializeField] [HideInInspector] private Animator _animator;
+
+        private bool _isInitialized;
+        public Animator FinalStateMachine => _animator;
+
+        private void OnValidate() => _animator = GetComponent<Animator>();
+
+        public void Construct()
+        {
+            if (AssertIsNotInitialized() is false) return;
+ 
+            foreach (var state in _animator.GetBehaviours<State>())
+            {
+                state.Construct(this);
+            }
+
+            _isInitialized = true;
+        }
+
+        private bool AssertIsInitialized()
+        {
+            var claim = $"{GetType().Name} is not initialized";
+            return _isInitialized ? true : throw new InvalidOperationException(claim);
+        }
+
+        private bool AssertIsNotInitialized()
+        {
+            var claim = $"{GetType().Name} is already initialized";
+            return !_isInitialized ? true : throw new InvalidOperationException(claim);
+        }
+
+        private bool DoesParameterExist(AnimatorControllerParameterType type, string candidate)
+        {
+            return _animator.parameters.Any(parameter => parameter.type == type && parameter.name == candidate);
+        }
+
+        void IController.Set(string trigger)
+        {
+            if (!AssertIsInitialized()) return;
+
+            if (!DoesParameterExist(AnimatorControllerParameterType.Trigger, trigger))
+            {
+                throw new UnityException($"{_animator.GetScenePath()} Trigger {trigger} not found!");
+            }
+            _animator.SetTrigger(trigger);
+        }
+
+        void IController.Set(string field, bool value)
+        {
+            if (!AssertIsInitialized()) return;
+
+            if (!DoesParameterExist(AnimatorControllerParameterType.Bool, field))
+            {
+                throw new UnityException($"{_animator.GetScenePath()} Bool {field} not found!");
+            }
+            _animator.SetBool(field, value);
+        }
+
+        void IController.Set(string field, int value)
+        {
+            if (!AssertIsInitialized()) return;
+
+            if (!DoesParameterExist(AnimatorControllerParameterType.Int, field))
+            {
+                throw new UnityException($"{_animator.GetScenePath()} Int {field} not found!");
+            }
+            _animator.SetInteger(field, value);
+        }
+
+        void IController.Set(string field, float value)
+        {
+            if (!AssertIsInitialized()) return;
+
+            if (!DoesParameterExist(AnimatorControllerParameterType.Float, field))
+            {
+                throw new UnityException($"{_animator.GetScenePath()} Float {field} not found!");
+            }
+            _animator.SetFloat(field, value);
+        }
+
+        public interface IController
+        {
+            Animator FinalStateMachine { get; }
+            void Set(string field, bool value);
+            void Set(string field, float value);
+            void Set(string field, int value);
+            void Set(string trigger);
+        }
+    }
+
+    internal static class AnimatorExtensions
+    {
+        public static string GetScenePath(this Animator _)
+        {
+            return "";
+        }
+    }
+}
