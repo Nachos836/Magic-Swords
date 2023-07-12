3420fafeae8057b2d85b9c99fe2d74591fc0f467
Cyka <nachos636@gmail.com>
2023-07-12 13:42:42 +0300

    [FEATURE] Add Dialog Scope

---
+++ Assets/DI/Dialog/Dependencies/AnimatedTextPresenterDependencies.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TMPro;
+using VContainer;
+using VContainer.Unity;
+
+namespace MagicSwords.DI.Dialog.Dependencies
+{
+    using Features.Dialog;
+    using Features.Dialog.Stages;
+    using Features.Generic.Sequencer;
+
+    internal static class AnimatedTextPresenterDependencies
+    {
+        public static IContainerBuilder AddAnimatedTextPresenter
+        (
+            this IContainerBuilder builder,
+            AnimatedTextPresenter presenter,
+            TextMeshProUGUI field,
+            float delay,
+            string[] monologue
+        ) {
+            builder.Register(resolver =>
+            {
+                var scope = resolver.CreateScope(scopeResolver =>
+                {
+                    scopeResolver.Register<Setup>(Lifetime.Transient)
+                        .WithParameter(field)
+                        .WithParameter(delay)
+                        .WithParameter(monologue)
+                        .AsSelf();
+
+                    scopeResolver.RegisterFactory<string, TextMeshProUGUI, TimeSpan, AutoPrint>(_ =>
+                    {
+                        return (currentSegment, currentField, currentDelay) =>
+                            new AutoPrint(currentSegment, currentField, currentDelay);
+
+                    }, Lifetime.Transient);
+                });
+
+                using (scope) return new Sequencer(firstState: scope.Resolve<Setup>());
+
+            }, Lifetime.Transient);
+
+            builder.RegisterComponent(presenter);
+
+            return builder;
+        }
+    }
+}
---
+++ Assets/DI/Dialog/DialogScope.cs
@@ -0,0 +1,25 @@
+﻿using MagicSwords.Features.Dialog;
+using TMPro;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace MagicSwords.DI.Dialog
+{
+    using Dependencies;
+
+    internal sealed class DialogScope : LifetimeScope
+    {
+        [field: SerializeField] public AnimatedTextPresenter Presenter { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI Field { get; private set; }
+        [field: SerializeField] public float Delay { get; private set; } = 0.05f;
+        [field: SerializeField] public string[] Monologue { get; private set; }
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+
+            builder.AddAnimatedTextPresenter(Presenter, Field, Delay, Monologue);
+        }
+    }
+}
