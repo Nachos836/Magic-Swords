592674b46ab5bfc6574ea194119348110b263def
Cyka <nachos636@gmail.com>
2023-07-14 16:55:23 +0300

    [CLEANUP] [HOTFIX] AnimatedText Sequence: Make resolution abstract for Initial Stage

--- Assets/Features/Dialog/Stages/Initial.cs
+++ Assets/Features/Dialog/Stages/Initial.cs
@@ -16,12 +16,12 @@ namespace MagicSwords.Features.Dialog.Stages
 
     public sealed class Initial : IStage, IStage.IProcess
     {
-        private readonly Func<Message, Print> _autoPrintResolver;
+        private readonly Func<Message, IStage> _resolveNext;
         private readonly string[] _monologue;
 
-        public Initial(Func<Message, Print> autoPrintResolver, string[] monologue)
+        public Initial(Func<Message, IStage> resolveNext, string[] monologue)
         {
-            _autoPrintResolver = autoPrintResolver;
+            _resolveNext = resolveNext;
             _monologue = monologue;
         }
 
@@ -29,7 +29,7 @@ namespace MagicSwords.Features.Dialog.Stages
         {
             if (cancellation.IsCancellationRequested) return new UniTask<Option>(Stage.Cancel);
 
-            return new UniTask<Option>(Option.From(_autoPrintResolver.Invoke(new Message(_monologue))));
+            return new UniTask<Option>(Option.From(_resolveNext.Invoke(new Message(_monologue))));
         }
     }
 }
