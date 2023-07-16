2a53d4d6441544121dc713058b2976d735b53b0d
Anastasia <timmikvo@gmail.com>
2023-07-16 11:00:14 +0300

    [HOTFIX] Add Skip Stage

---
+++ Assets/Features/Dialog/Stages/Skip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+
+namespace MagicSwords.Features.Dialog.Stages
+{
+    using Payload;
+    using Generic.Sequencer;
+    using Option = Generic.Functional.OneOf
+    <
+        Generic.Sequencer.IStage,
+        Generic.Sequencer.Stage.Canceled,
+        Generic.Sequencer.Stage.Errored
+    >;
+
+    public sealed class Skip : IStage, IStage.IProcess
+    {
+        private readonly Func<Message, IStage> _resolveNext;
+        private readonly Message _message;
+        private readonly TextMeshProUGUI _text;
+
+        public Skip(Func<Message, IStage> resolveNext, Message message, TextMeshProUGUI text)
+        {
+            _resolveNext = resolveNext;
+            _message = message;
+            _text = text;
+        }
+
+        async UniTask<Option> IStage.IProcess.ProcessAsync(CancellationToken cancellation)
+        {
+            if (cancellation.IsCancellationRequested) return Stage.Cancel;
+
+            _text.text = _message.Part;
+            if (await UniTask.Delay(1000, cancellationToken: cancellation).SuppressCancellationThrow())
+                return Stage.Cancel;
+            return Option.From(_resolveNext.Invoke(_message));
+        }
+    }
+}
