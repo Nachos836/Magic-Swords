f46e94fd29effe59f83c1202b8d176575bf091b3
Anastasia <timmikvo@gmail.com>
2023-08-09 12:55:44 +0300

    [FEATURE] Add text animator implementation

---
+++ Assets/Features/TextAnimator/TagSequence.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+
+namespace MagicSwords.Features.TextAnimator
+{
+    public struct TagSequence
+    {
+        private readonly string _letters;
+        private readonly IEffect _effect;
+
+        public TagSequence(string letters, IEffect effect)
+        {
+            _letters = letters;
+            _effect = effect;
+        }
+
+        public async UniTask ShowSequence(CancellationToken cancellation)
+        {
+            if (_effect is IWordEffect wordEffect)
+            {
+                var words = _letters.Split();
+                foreach (var word in words)
+                {
+                    await wordEffect.RunAsync(word,cancellation);
+                    
+                }
+            }
+            else if (_effect is ICharEffect charEffect)
+            {
+                foreach (var letter in _letters)
+                {
+                    await charEffect.RunAsync(letter,cancellation);
+                    
+                }
+            }
+            else if (_effect is IRegularEffect regularEffect)
+            {
+                await regularEffect.RunAsync(_letters,cancellation);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+
+    public interface IRegularEffect
+    {
+        Task RunAsync(string letters, CancellationToken cancellation);
+    }
+
+    public interface ICharEffect
+    {
+        Task RunAsync(char letter, CancellationToken cancellation);
+    }
+
+    public interface IWordEffect
+    {
+        Task RunAsync(string word, CancellationToken cancellation);
+    }
+
+    public interface IEffect
+    {
+    }
+}
---
+++ Assets/Features/TextAnimator/TextAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MagicSwords.Features.TextAnimator
+{
+    public sealed class TextAnimator : MonoBehaviour
+    {
+        private List<TagSequence> _tags = new();
+
+        // text = "<wobble>Cyka</>"
+        public async UniTask PresentAsync(string text,CancellationToken cancellation=default)
+        {
+            _tags.AddRange(BuildTags(text,cancellation));
+            foreach (var tag in _tags)
+            {
+                await tag.ShowSequence(cancellation);
+            }
+        }
+
+        // text = "<wobble>Cyka</>"
+        private IEnumerable<TagSequence> BuildTags(string text, CancellationToken cancellation)
+        {
+            int startTag = 0;
+            int endTag = 0;
+            IEffect effect = default;
+            var letters = text[startTag..endTag];
+            
+            yield return new TagSequence(letters,effect);
+            /*
+             * Letters = "Cyka"
+             * Effect = new Wobble()
+             */
+        }
+    }
+}
