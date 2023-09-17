e55a561159bd118267dbc84d0bd83d211ef30585
Cyka <nachos636@gmail.com>
2023-09-17 07:58:19 +0300

    [FEATURE] Extend functional with Optional Monad

---
+++ Assets/Features/Generic/Functional/Optional.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using JetBrains.Annotations;
+using Unity.Burst;
+
+namespace MagicSwords.Features.Generic.Functional
+{
+    [BurstCompile]
+    public readonly struct Optional<T>
+    {
+        [CanBeNull] private readonly T _value;
+
+        public Optional(T value) => _value = value;
+
+        public static Optional<T> None { get; }  = new();
+        public static Optional<T> Some(T value) => new (value);
+
+        public void Match(Action<T> some, Action none)
+        {
+            if (_value is not null)
+            {
+                some.Invoke(_value);
+            }
+            else
+            {
+                none.Invoke();
+            }
+        }
+
+        public Optional<T> Run(Func<T, T> transformation)
+        {
+            return _value is not null
+                ? new Optional<T>(transformation.Invoke(_value))
+                : None;
+        }
+
+        public void Run(Action<T> transformation)
+        {
+            if (_value is not null) transformation.Invoke(_value);
+        }
+    }
+}
--- Assets/Features/Generic/Functional/Outcome/Expected.cs
+++ Assets/Features/Generic/Functional/Outcome/Expected.cs
@@ -5,12 +5,14 @@ namespace MagicSwords.Features.Generic.Functional.Outcome
     public static class Expected
     {
         private static readonly Lazy<Success> LazySuccess = new (() => new Success());
+        private static readonly Lazy<Unit> LazyUnit = new (() => new Unit());
 
         private static readonly Lazy<Abortion> LazyAborted = new (() => new Abortion());
         private static readonly Lazy<Failure> LazyFailed= new (() => new Failure());
         private static readonly Lazy<Cancellation> LazyCancel = new (() => new Cancellation());
 
         public static Success Success { get; } = LazySuccess.Value;
+        public static Unit Unit { get; } = LazyUnit.Value;
 
         public static Abortion Aborted { get; } = LazyAborted.Value;
         public static Failure Failed { get; } = LazyFailed.Value;
