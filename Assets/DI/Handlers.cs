4351de8373f59710fc4cdcd8d7f45c7a1814b7e4
Cyka <nachos636@gmail.com>
2023-09-24 12:23:39 +0300

    [ME-47] [FEATURE] Add Default DI Exception Handler

---
+++ Assets/DI/Handlers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace MagicSwords.DI
+{
+    internal static class Handlers
+    {
+        public static void DefaultExceptionHandler(Exception exception)
+        {
+#           if DEBUG
+            Debug.LogException(exception);
+#           else
+
+#           endif
+        }
+    }
+}
