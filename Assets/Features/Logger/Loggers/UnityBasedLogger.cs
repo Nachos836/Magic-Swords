7ec27202550c907d3732a33ceb0babd531815276
Cyka <nachos636@gmail.com>
2023-09-24 17:31:29 +0300

    [ME-94] [FEATURE] Add Logger feature implementation

---
+++ Assets/Features/Logger/Loggers/UnityBasedLogger.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace MagicSwords.Features.Logger.Loggers
+{
+    internal sealed class UnityBasedLogger : ILogger
+    {
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogInformation(Object context, string format, params object[] arguments)
+        {
+            Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, context, format, arguments);
+        }
+
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogWarning(Object context, string format, params object[] arguments)
+        {
+            Debug.LogFormat(LogType.Warning, LogOption.NoStacktrace, context, format, arguments);
+        }
+
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogError(Object context, string format, params object[] arguments)
+        {
+            Debug.LogFormat(LogType.Error, LogOption.None, context, format, arguments);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogException(Object context, System.Exception exception)
+        {
+            Debug.LogException(exception, context);
+        }
+    }
+}
---
+++ Assets/Features/Logger/Loggers/VoidLogger.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace MagicSwords.Features.Logger.Loggers
+{
+    internal sealed class VoidLogger : ILogger
+    {
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogInformation(Object context, string format, params object[] arguments) { }
+
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogWarning(Object context, string format, params object[] arguments) { }
+
+        [StringFormatMethod("format")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogError(Object context, string format, params object[] arguments) { }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ILogger.LogException(Object context, System.Exception exception) { }
+    }
+}
