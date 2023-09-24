e0e15379de26fe818c4ca26cad93966d7635c9aa
Cyka <nachos636@gmail.com>
2023-09-24 18:56:04 +0300

    [ME-94] [HOTFIX] Make use of ILogger in Main Menu Scope

--- Assets/Features/MainMenu/MainMenuEntryPoint.cs
+++ Assets/Features/MainMenu/MainMenuEntryPoint.cs
@@ -1,15 +1,23 @@
 ﻿using System.Threading;
 using Cysharp.Threading.Tasks;
-using UnityEngine;
 using VContainer.Unity;
 
 namespace MagicSwords.Features.MainMenu
 {
+    using Logger;
+
     internal sealed class MainMenuEntryPoint : IAsyncStartable
     {
+        private readonly ILogger _logger;
+
+        public MainMenuEntryPoint(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public UniTask StartAsync(CancellationToken cancellation)
         {
-            Debug.Log("Вот наше главное меню!");
+            _logger.LogInformation("Вот наше главное меню!");
 
             return UniTask.CompletedTask;
         }
