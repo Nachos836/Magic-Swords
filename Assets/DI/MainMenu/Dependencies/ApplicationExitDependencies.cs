ad203401b21b57fdfa121e8b702ed71a847948bd
Cyka <nachos636@gmail.com>
2023-09-25 17:08:00 +0300

    [ME-95] [HOTFIX] Adjust MainMenuScope flow

---
+++ Assets/DI/MainMenu/Dependencies/ApplicationExitDependencies.cs
@@ -0,0 +1,25 @@
+﻿using VContainer;
+
+namespace MagicSwords.DI.MainMenu.Dependencies
+{
+    using Features.ApplicationExit;
+    using Features.ApplicationExit.Routines;
+
+    internal static class ApplicationExitDependencies
+    {
+        public static IContainerBuilder AddApplicationExitRoutine(this IContainerBuilder builder)
+        {
+            builder.Register<IApplicationExitRoutine>(_ =>
+            {
+#           if UNITY_EDITOR
+                return new RuntimeExitRoutine();
+#           else
+                return new PlaymodeExitRoutine();
+#           endif
+
+            }, Lifetime.Scoped);
+
+            return builder;
+        }
+    }
+}
---
+++ Assets/DI/MainMenu/Dependencies/MainMenuViewModelDependencies.cs
@@ -0,0 +1,19 @@
+﻿using VContainer;
+
+namespace MagicSwords.DI.MainMenu.Dependencies
+{
+    using Features.MainMenu;
+
+    internal static class MainMenuViewModelDependencies
+    {
+        public static IContainerBuilder AddMainMenuViewModel(this IContainerBuilder builder, MainMenuViewModel mainMenuViewModel)
+        {
+            builder.RegisterBuildCallback(container =>
+            {
+                container.Inject(mainMenuViewModel);
+            });
+
+            return builder;
+        }
+    }
+}
