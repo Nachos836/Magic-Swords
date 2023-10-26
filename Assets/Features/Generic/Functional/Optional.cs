a22c5a79691e7f41c3d4b41e29fb7e00d42b1ad0
Cyka <nachos636@gmail.com>
2023-10-26 16:52:26 +0300

    [ME-5] [HOTFIX] Extend Optional Monad

--- Assets/Features/Generic/Functional/Optional.cs
+++ Assets/Features/Generic/Functional/Optional.cs
@@ -1,40 +1,58 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Unity.Burst;
 
 namespace MagicSwords.Features.Generic.Functional
 {
     [BurstCompile]
-    public readonly struct Optional<T>
+    public readonly struct Optional<TValue>
     {
-        private readonly T? _value;
+        private readonly TValue? _value;
+        private readonly bool _hasSome;
 
-        private Optional(T? value) => _value = value;
-
-        public static Optional<T> None { get; } = new ();
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Optional<T> Some(T? value) => new (value);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Run(Action<T> transformation)
+        private Optional(TValue? value)
         {
-            if (_value is not null) transformation.Invoke(_value);
+            _value = value;
+
+            _hasSome = _value is not null;
+        }
+
+        public static Optional<TValue> None { get; } = new ();
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Optional<TValue> Some(TValue? value) => new (value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Optional<TValue> (TValue value) => new (value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Optional<TValue, TAnother> Attach<TAnother>(TAnother? another)
+        {
+            return _hasSome
+                ? Optional<TValue, TAnother>.Some(_value, another)
+                : Optional<TValue, TAnother>.None;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Optional<T> Run(Func<T, T> transformation)
+        public void Run(Action<TValue> transformation)
         {
-            return _value is not null
-                ? Some(transformation.Invoke(_value))
+            if (_hasSome) transformation.Invoke(_value!);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Optional<TValue> Run(Func<TValue, TValue> transformation)
+        {
+            return _hasSome
+                ? Some(transformation.Invoke(_value!))
                 : this;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Match(Action<T> some, Action none)
+        public void Match(Action<TValue> some, Action none)
         {
-            if (_value is not null)
+            if (_hasSome)
             {
-                some.Invoke(_value);
+                some.Invoke(_value!);
             }
             else
             {
@@ -43,10 +61,82 @@ namespace MagicSwords.Features.Generic.Functional
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public TMatch Match<TMatch>(Func<T, TMatch> some, Func<TMatch> none)
+        public TMatch Match<TMatch>(Func<TValue, TMatch> some, Func<TMatch> none)
         {
-            return _value is not null
-                ? some.Invoke(_value)
+            return _hasSome
+                ? some.Invoke(_value!)
+                : none.Invoke();
+        }
+    }
+
+    [BurstCompile]
+    public readonly struct Optional<TFirst, TSecond>
+    {
+        private readonly TFirst? _first;
+        private readonly TSecond? _second;
+        private readonly bool _hasSome;
+
+        private Optional(TFirst? first, TSecond? second)
+        {
+            (_first, _second) = (first, second);
+
+            _hasSome = _first is not null & _second is not null;
+        }
+
+        public static Optional<TFirst, TSecond> None { get; } = new ();
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Optional<TFirst, TSecond> Some(TFirst? value, TSecond? second) => new (value, second);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Optional<TFirst, TSecond>((TFirst? First, TSecond? Second) value)
+        {
+            return new Optional<TFirst, TSecond>(value.First, value.Second);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Run(Action<TFirst, TSecond> transformation)
+        {
+            if (_hasSome) transformation.Invoke(_first!, _second!);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Optional<TFirst, TSecond> Run(Func<TFirst, TSecond, (TFirst First, TSecond Second)> transformation)
+        {
+            return _hasSome
+                ? transformation.Invoke(_first!, _second!)
+                : this;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public AsyncResult<TAnother> Run<TAnother>
+        (
+            Func<TFirst, TSecond, CancellationToken, AsyncResult<TAnother>> whenSome,
+            Func<CancellationToken, AsyncResult<TAnother>> whenNone,
+            CancellationToken cancellation
+        ) {
+            return _hasSome
+                ? whenSome.Invoke(_first!, _second!, cancellation)
+                : whenNone.Invoke(cancellation);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Match(Action<TFirst, TSecond> some, Action none)
+        {
+            if (_hasSome)
+            {
+                some.Invoke(_first!, _second!);
+            }
+            else
+            {
+                none.Invoke();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TMatch Match<TMatch>(Func<TFirst, TSecond, TMatch> some, Func<TMatch> none)
+        {
+            return _hasSome
+                ? some.Invoke(_first!, _second!)
                 : none.Invoke();
         }
     }
