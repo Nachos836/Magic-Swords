c08dff34274d103616f133fe32c6d3a8add0c679
Cyka <nachos636@gmail.com>
2023-11-10 18:54:59 +0200

    [ME-5] [CLEANUP] ITextDisplayingListener

--- Assets/Features/Text/AnimatedRichText/Playing/ITextPlayer.cs
+++ Assets/Features/Text/AnimatedRichText/Playing/ITextPlayer.cs
@@ -4,15 +4,9 @@ using Cysharp.Threading.Tasks;
 namespace MagicSwords.Features.Text.AnimatedRichText.Playing
 {
     using Generic.Functional;
-    using Generic.Functional.Outcome;
 
     internal interface ITextPlayer
     {
         UniTask<AsyncResult<DissolveAnimationsHandler>> PlayAsync(CancellationToken cancellation = default);
-    }
-
-    internal interface ITextDisplayingListener
-    {
-        IUniTaskAsyncEnumerable<Unit> DisplayingStreamAsync(CancellationToken cancellation = default);
     }
 }
